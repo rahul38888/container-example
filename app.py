a83fb8577f538e3d3c97074519d12df9f637fde3
rahul38888 <rahul38888@gmail.com>
2024-05-06 17:05:54 +0530

    Basic python API service with Dockerfile: v3

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import datetime
+import logging
+import logging.config
+import socket
+
 from flask import Flask
 from waitress import serve
 
@@ -6,17 +10,25 @@ app = Flask(__name__)
 
 last_visit_map = {}
 
+logging.config.fileConfig("logging.ini")
+logger = logging.getLogger("main")
+
+host_ip = socket.gethostbyname(socket.gethostname())
+
 
 @app.route("/ping")
 def ping():
+    logger.info("Ping api triggered")
     return "Pong"
 
 
 @app.route("/profile/<name>")
 def profile(name: str):
-    last_visit = "This is your first visit!"
+    logger.info(f"Profile api triggered by {name}")
+    last_visit = f"This is your first visit on <b>{host_ip}</b>\n"
     if last_visit_map.__contains__(name):
-        last_visit = "Your last visit was at " + last_visit_map.get(name).strftime("%H:%M:%S on %d/%M/%Y") + "!"
+        last_time = last_visit_map.get(name).strftime("%H:%M:%S on %d/%M/%Y")
+        last_visit = f"Your last visit time on <b>{host_ip}</b> was <b>{last_time}</b>!\n"
 
     last_visit_map[name] = datetime.datetime.now()
     return f"Welcome to you profile {name}. {last_visit}"
